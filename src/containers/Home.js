1f31a0b2663f4ed6af50ba02b2e7878a5f71a384
MatthewLoffredo <mattloffredo808@gmail.com>
2020-04-17 18:58:23 -0400

    Integrated Iqra's changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,11 +4,11 @@ import './Home.css';
 //home page
 export default function Home() {
   return (
-    <div className="Home">
-      <div className="maintext">
-        <h1>Ramblerfy</h1>
-        <p>Interactive Spotify Webapp</p>
-      </div>
+    <div className="main-content-header">
+        <h1>WELCOME TO <span className="colorchange">RAMBLERFY!</span><br />
+            WHERE YOU FIND THE BEST JAMS </h1>
+        <a href="#" className="btn btn-one">SOMETHING</a>
+        <a href="#" className="btn btn-two">SHOW ME MORE</a>
     </div>
   );
 }

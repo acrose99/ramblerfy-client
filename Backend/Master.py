b5f9ee78e7da58914deb6b29aad5eb84b2dc6c72
MatthewLoffredo <mattloffredo808@gmail.com>
2020-04-28 20:12:06 -0500

    Added Alex's Backend Files

---
+++ Backend/Master.py
@@ -0,0 +1,55 @@
+import time
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# connects to the API using the official RamblerFy api credentials I set up
+import audio_features
+import search
+
+
+def main():
+    client_credentials_manager = SpotifyClientCredentials(client_id="318bd146dc0b46bd9e41e990bbe4ce2a",
+                                                          client_secret="c56fe1ad00454247b966648148405c19",
+                                                          proxies=None,
+                                                          requests_timeout=None)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # Turn this to TRUE to see all the HTTP exchanges between us and the API
+    # Turn this to False for pretty output
+    sp.trace = False
+    search_string = str(input("What would you like to search for?"))
+    search_type = str(input("Is this an artist, song, album, playlist?"))
+
+    # For seeing how long each call takes
+    start = time.time()
+
+    search.main(sp, search_string, search_type)
+    # audio_features.print_json(search_string)
+
+    delta = time.time() - start
+    print("Data retrieved in %.2f seconds" % (delta,))
+
+
+def script():
+    client_credentials_manager = SpotifyClientCredentials(client_id="318bd146dc0b46bd9e41e990bbe4ce2a",
+                                                          client_secret="c56fe1ad00454247b966648148405c19",
+                                                          proxies=None,
+                                                          requests_timeout=None)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # Turn this to TRUE to see all the HTTP exchanges between us and the API
+    # Turn this to False for pretty output
+    sp.trace = False
+    search_type = "artist"
+    with open('C:/Users/Matt/ramblerfy-backend/ramblerfy-client/Backend/output.txt') as f:
+        artists = []
+        for line in f:
+            artists.append(line)
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    artists = [x.strip() for x in artists]
+    search.search_multiples(sp, artists, search_type)
+
+
+main()
+# script()
---
+++ Backend/dbQuery.js
@@ -0,0 +1,290 @@
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(cors());
+
+// this is our MongoDB database
+// connects our back end code with the database
+const dbUri = "mongodb+srv://arose5:ZaraYaqob14$3@ramblerpy-5rd9x.mongodb.net/test?retryWrites=true&w=majority"
+// const client = new MongoClient(dbUri, {useNewUrlParser: true});
+
+//Returns 5 random songs based on the user filters filterLow and filterHigh
+
+
+//TODO For ALL:
+//TODO: Ask for User input of filterLow and filterHigh
+//TODO: Create handling of repeat songs OR have better randomization
+//TODO: Optimize performance by possibly using Mongo's built in filters (I dont think they will work based of the structure of the doc)
+
+function searchDb() {
+  MongoClient.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true  }, function(err, db) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    var dbo = db.db("Spotify_Tracks");
+    dbo.collection("Tracks").aggregate([
+       {$match: {'tracks.track_name': 'Rihanna'}},
+       {$project: {
+          tracks: {$filter: {
+              input: '$tracks',
+              as: 'track',
+              cond: {$eq: ['$$track.track_name', 'Rihanna']}
+          }},
+        _id: 0
+      }}
+    ])
+    .toArray(function(err, result) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      console.log(result[0]);
+      db.close();
+    });
+    // dbo.collection("Tracks").find(
+    //   { "tracks.track_name": "Rihanna" }
+    //   ).toArray(function(err, result) {
+    //   if (err) {
+    //     console.log(err);
+    //     throw err;
+    //   }
+    //   console.log(result);
+    //   db.close();
+    // });
+  });
+}
+
+searchDb();
+//
+// function getDanceabilty(filterLow, filterHigh) {
+//     client.connect(err => {
+//         const collection = client.db("Spotify_Tracks").collection("Tracks");
+//         client.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//         collection.find('tracks').toArray(function (err, data) {
+//         }
+//         client.close()
+//         // perform actions on the collection object
+//     })
+// }
+//
+// function getValence(filterLow, filterHigh) {
+//     client.connect(err => {
+//         const collection = client.db("Spotify_Tracks").collection("Tracks");
+//         client.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//         collection.find('tracks').toArray(function (err, data) {
+//             const songs = data[0]['tracks'];
+//             // console.log(songs)
+//             var high = parseInt(Math.random() * (22539 - 100) + 100);
+//             // console.log(high);
+//             var low = high - 100;
+//             // console.log(low);
+//             let song;
+//             let wantedsongs = [];
+//             // TODO Ask for User input in UI
+//             // var filterLow = prompt("What low value do you want?");
+//             // var filterHigh = prompt("What high value do you want?");
+//             let filterLow = .0;
+//             let filterHigh = .2;
+//             while (low <= high) {
+//                 song = songs[low];
+//                 const valence = song['valence:'];
+//                 // console.log(valence);
+//                 if (valence >= filterLow && valence <= filterHigh) {
+//                     if (wantedsongs.length > 0) {
+//                         if (wantedsongs[wantedsongs.length-1]['track_artist'] !== song['track_artist']) {
+//                             wantedsongs.push(song)
+//                         }
+//                         else if (wantedsongs[wantedsongs.length-1]['track_artist'] === song['track_artist']) {
+//                             console.log('Repeat Artist');
+//                         }
+//                     }
+//                     else if (wantedsongs.length === 0) {
+//                         wantedsongs.push(song)
+//                     }
+//                     else {
+//                         console.log('Error');
+//                     }
+//                 }
+//                 low++;
+//             }
+//             var i
+//             for (i = 0; i < 5; i++) {
+//                 try {
+//                     let cover_image_url = wantedsongs[i]['cover_image_url'];
+//                     let spotify_url = wantedsongs[i]['spotify_url'];
+//                     let track_album = wantedsongs[i]['track_album'];
+//                     let track_artist = wantedsongs[i]['track_artist'];
+//                     let track_name = wantedsongs[i]['track_name'];
+//                     let valence = wantedsongs[i]['valence:'];
+//                     console.log("COVER IMAGE: " + cover_image_url);
+//                     console.log("SPOTIFY URL: " + spotify_url);
+//                     console.log("ALBUM: " + track_album);
+//                     console.log("ARTIST:" + track_artist);
+//                     console.log("SONG: " + track_name);
+//                     console.log("VALENCE: " + valence);
+//                     console.log('--------------------------------');
+//                 } catch (e) {
+//                     console.log('Error with this song');
+//                 }
+//                 {
+//
+//                 }
+//             }
+//         })
+//         client.close()
+//         // perform actions on the collection object
+//     });
+// }
+//
+// function getDuration(filterLow, filterHigh) {
+//     client.connect(err => {
+//         const collection = client.db("Spotify_Tracks").collection("Tracks");
+//         client.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//         collection.find('tracks').toArray(function (err, data) {
+//             const songs = data[0]['tracks'];
+//             // console.log(songs)
+//             var high = parseInt(Math.random() * (22539 - 100) + 100);
+//             // console.log(high);
+//             var low = high - 100;
+//             // console.log(low);
+//             let song;
+//             let wantedsongs = [];
+//             // TODO Ask for User input in UI
+//             // var filterLow = prompt("What low value do you want?");
+//             // var filterHigh = prompt("What high value do you want?");
+//             let filterLow = 2.0;
+//             let filterHigh = 6.0;
+//             while (low <= high) {
+//                 song = songs[low];
+//                 const duration = song['duration_ms'];
+//                 if (duration >= filterLow * 60000 && duration <= filterHigh * 60000) {
+//                     // console.log(song['duration_ms']);
+//                     if (wantedsongs.length > 0) {
+//                         if (wantedsongs[wantedsongs.length-1]['track_artist'] !== song['track_artist']) {
+//                             wantedsongs.push(song)
+//                         }
+//                         else if (wantedsongs[wantedsongs.length-1]['track_artist'] === song['track_artist']) {
+//                             console.log('Repeat Artist');
+//                         }
+//                     }
+//                     else if (wantedsongs.length === 0) {
+//                         wantedsongs.push(song)
+//                     }
+//                     else {
+//                         console.log('Error');
+//                     }
+//                 }
+//                 low++;
+//             }
+//             var i
+//             for (i = 0; i < 5; i++) {
+//                 try {
+//                     let cover_image_url = wantedsongs[i]['cover_image_url'];
+//                     let spotify_url = wantedsongs[i]['spotify_url'];
+//                     let track_album = wantedsongs[i]['track_album'];
+//                     let track_artist = wantedsongs[i]['track_artist'];
+//                     let track_name = wantedsongs[i]['track_name'];
+//                     let duration = wantedsongs[i]['duration_ms'];
+//                     var minutes = Math.floor(duration / 60000);
+//                     var seconds = ((duration % 60000) / 1000).toFixed(0);
+//                     duration = minutes + ":" + (seconds < 10 ? '' : '') + seconds;
+//                     console.log("COVER IMAGE: " + cover_image_url);
+//                     console.log("SPOTIFY URL: " + spotify_url);
+//                     console.log("ALBUM: " + track_album);
+//                     console.log("ARTIST:" + track_artist);
+//                     console.log("SONG: " + track_name);
+//                     console.log("DURATION: " + duration);
+//                     console.log('--------------------------------');
+//                 } catch (e) {
+//                     console.log('Error with this song');
+//                 }
+//                 {
+//
+//                 }
+//             }
+//         })
+//         client.close()
+//         // perform actions on the collection object
+//     });
+// }
+//
+// function getTempo(filterLow, filterHigh) {
+//     const dbUri = "mongodb+srv://arose5:ZaraYaqob14$3@ramblerpy-5rd9x.mongodb.net/test?retryWrites=true&w=majority"
+//     const client = new MongoClient(dbUri, {useNewUrlParser: true});
+//     client.connect(err => {
+//         const collection = client.db("Spotify_Tracks").collection("Tracks");
+//         client.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//         collection.find('tracks').toArray(function (err, data) {
+//             const songs = data[0]['tracks'];
+//             // console.log(songs)
+//             var high = parseInt(Math.random() * (22539 - 100) + 100);
+//             // console.log(high);
+//             var low = high - 100;
+//             // console.log(low);
+//             let song;
+//             let wantedsongs = [];
+//             // TODO Ask for User input in UI
+//             // var filterLow = prompt("What low value do you want?");
+//             // var filterHigh = prompt("What high value do you want?");
+//             let filterLow = 100.0; //we should make some limits for this (After looking at 500 songs, 75 looks good)
+//             let filterHigh = 150.0; ////we should make some limits for this (After looking at 500 songs, 180-185 looks good)
+//             while (low <= high) {
+//                 song = songs[low];
+//                 const tempo = song['tempo'];
+//                 // console.log(tempo);
+//                 if (tempo >= filterLow && tempo <= filterHigh) {
+//                     if (wantedsongs.length > 0) {
+//                         if (wantedsongs[wantedsongs.length-1]['track_artist'] !== song['track_artist']) {
+//                             wantedsongs.push(song)
+//                         }
+//                         else if (wantedsongs[wantedsongs.length-1]['track_artist'] === song['track_artist']) {
+//                             console.log('Repeat Artist');
+//                         }
+//                     }
+//                     else if (wantedsongs.length === 0) {
+//                         wantedsongs.push(song)
+//                     }
+//                     else {
+//                         console.log('Error');
+//                     }
+//                 }
+//                 low++;
+//             }
+//             var i
+//             for (i = 0; i < 5; i++) {
+//                 try {
+//                     let cover_image_url = wantedsongs[i]['cover_image_url'];
+//                     let spotify_url = wantedsongs[i]['spotify_url'];
+//                     let track_album = wantedsongs[i]['track_album'];
+//                     let track_artist = wantedsongs[i]['track_artist'];
+//                     let track_name = wantedsongs[i]['track_name'];
+//                     let tempo = wantedsongs[i]['tempo'];
+//                     console.log("COVER IMAGE: " + cover_image_url);
+//                     console.log("SPOTIFY URL: " + spotify_url);
+//                     console.log("ALBUM: " + track_album);
+//                     console.log("ARTIST:" + track_artist);
+//                     console.log("SONG: " + track_name);
+//                     console.log("TEMPO: " + tempo);
+//                     console.log('--------------------------------');
+//                 } catch (e) {
+//                     console.log('Error with this song');
+//                 }
+//                 {
+//
+//                 }
+//             }
+//         })
+//         // perform actions on the collection object
+//         client.close()
+//     });
+// }
+//
+// module.exports = {
+//
+//     getDanceabilty,
+//     getTempo,
+//     getDuration,
+//     getValence
+// }

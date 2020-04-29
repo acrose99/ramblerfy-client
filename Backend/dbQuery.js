a5fd747e28c40a942da97a2b06ec8bfb25864f29
Alexander Rose <acrosemail@gmail.com>
2020-04-29 00:43:14 -0500

    Added MongoDB filters for one input or two input sliders, we need the User input and Handling for repeats still

--- Backend/dbQuery.js
+++ Backend/dbQuery.js
@@ -15,12 +15,10 @@ const client = new MongoClient(dbUri, {useNewUrlParser: true});
 //Returns 5 random songs based on the user filters filterLow and filterHigh
 
 
-//TODO For ALL:
 //TODO: Ask for User input of filterLow and filterHigh
 //TODO: Create handling of repeat songs OR have better randomization
-//TODO: Optimize performance by possibly using Mongo's built in filters (I dont think they will work based of the structure of the doc)
-
 function searchDb(filterDanceabiltyLow, filterDanceabiltyHigh, filterValenceLow, filterValenceHigh, filterTempoLow, filterTempoHigh,filterDurationLow, filterDurationHigh)  {
+    let filteredSongs = [];
   MongoClient.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true  }, function(err, db) {
     if (err) {
       console.log(err);
@@ -60,47 +58,53 @@ function searchDb(filterDanceabiltyLow, filterDanceabiltyHigh, filterValenceLow,
               }}
       ])
     .toArray(function(err, result) {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
+        if (err) {
+            console.log(err);
+            throw err;
+        }
         //only stores the JSON
-        filteredSongs = result[0]['tracks']
+        filteredSongs = result[0]['tracks'];
+        console.log(filteredSongs);
 
+        //Returns all the songs for UI function
+        db.close();
+        return filteredSongs;
       //we want to display 5 songs each search
 
-      if (result[0].length >= 5) {
-          console.log("Please broaden your search variables!")
-      }
-      else{
-          for (i = 0; i < 5; i ++) {
-              let cover_image_url = filteredSongs[i]['cover_image_url'];
-              let spotify_url = filteredSongs[i]['spotify_url'];
-              let track_album = filteredSongs[i]['track_album'];
-              let track_artist = filteredSongs[i]['track_artist'];
-              let track_name = filteredSongs[i]['track_name'];
-              let duration_ms = filteredSongs[i]['duration_ms'];
-              let danceability = filteredSongs[i]['danceability'];
-              let valence = filteredSongs[i]['valence:'];
-              let tempo = filteredSongs[i]['tempo'];
-              console.log("COVER IMAGE: " + cover_image_url);
-              console.log("SPOTIFY URL: " + spotify_url);
-              console.log("ALBUM: " + track_album);
-              console.log("ARTIST:" + track_artist);
-              console.log("SONG: " + track_name);
-              var minutes = Math.floor(duration_ms / 60000);
-              var seconds = ((duration_ms % 60000) / 1000).toFixed(0);
-              var duration = minutes + ":" + (seconds < 10 ? '' : '') + seconds;
-              console.log("DURATION: " + duration);
-              console.log("DANCEABILITY: " + danceability);
-              console.log("VALENCE: " + valence);
-              console.log("TEMPO: " + tempo);
-              console.log('--------------------------------');
-          }
-      }
-      db.close();
+      // if (result[0].length >= 5) {
+      //     console.log("Please broaden your search variables!")
+      // }
+      // else{
+      //     for (i = 0; i < 5; i ++) {
+      //         let cover_image_url = filteredSongs[i]['cover_image_url'];
+      //         let spotify_url = filteredSongs[i]['spotify_url'];
+      //         let track_album = filteredSongs[i]['track_album'];
+      //         let track_artist = filteredSongs[i]['track_artist'];
+      //         let track_name = filteredSongs[i]['track_name'];
+      //         let duration_ms = filteredSongs[i]['duration_ms'];
+      //         let danceability = filteredSongs[i]['danceability'];
+      //         let valence = filteredSongs[i]['valence:'];
+      //         let tempo = filteredSongs[i]['tempo'];
+      //         console.log("COVER IMAGE: " + cover_image_url);
+      //         console.log("SPOTIFY URL: " + spotify_url);
+      //         console.log("ALBUM: " + track_album);
+      //         console.log("ARTIST:" + track_artist);
+      //         console.log("SONG: " + track_name);
+      //         var minutes = Math.floor(duration_ms / 60000);
+      //         var seconds = ((duration_ms % 60000) / 1000).toFixed(0);
+      //         var duration = minutes + ":" + (seconds < 10 ? '' : '') + seconds;
+      //         console.log("DURATION: " + duration);
+      //         console.log("DANCEABILITY: " + danceability);
+      //         console.log("VALENCE: " + valence);
+      //         console.log("TEMPO: " + tempo);
+      //         console.log('--------------------------------');
+      //     }
+      // }
+      // db.close();
     });
   });
+    // console.log(filteredSongs);
+    // return filteredSongs;
 }
 
 // searchDb();
